079186e571eb0c6227539ffb23b84cf0d4ed4de1
iclubstoree <iclubstoree@gmail.com>
2025-07-06 16:17:02 -0300

    painel atualizado

---
+++ api/firebase.js
@@ -0,0 +1,1 @@
+// Firebase config for painel-iclub-novo
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,1 @@
-const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "painel-iclub.firebaseapp.com",
-  projectId: "painel-iclub",
-  storageBucket: "painel-iclub.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
-};
+// Firebase config aqui
